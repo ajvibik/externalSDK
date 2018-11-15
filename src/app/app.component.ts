053938a7be45c559aef15049229938f8d0ed9b55
ajvibik <ajvibik@gmail.com>
2018-11-15 11:17:25 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarBarcodeEncoderService } from './service/star-barcode-encoder.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'externalSDK';
+  constructor(private starBarcodeEncoderService:StarBarcodeEncoderService){
+
+  }
+   ngOnInit() {
+    
+   }
+   getValue(a){
+    this.starBarcodeEncoderService.createUPCE(a);
+   }
 }
